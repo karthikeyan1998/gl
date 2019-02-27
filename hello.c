c9035058c7a6b4b1e32efd824a71da813a9a4698
karthikeyan1998 <47741331+karthikeyan1998@users.noreply.github.com>
2019-02-27 16:24:15 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    printf("enter the times");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("\n hello");
+    }
+    getch();
+    }
